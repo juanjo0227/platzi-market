f7dedacf445bfd876cfd2ea1961db61a0f898be3
Juan José <jjrd91@hotmail.com>
2020-12-28 07:54:03 -0500

    Creación de Query Methods

---
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,13 @@
+package com.platzi.market.persistence.crud;
+
+import com.platzi.market.persistence.entity.Producto;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+}
--- src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
@@ -9,12 +9,19 @@ public class ComprasProducto {
     @EmbeddedId
     private ComprasProductoPK id;
 
-
     private Integer cantidad;
 
     private Double total;
 
     private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compra", insertable = false, updatable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", insertable = false, updatable = false)
+    private Producto producto;
 
     public ComprasProductoPK getId() {
         return id;
